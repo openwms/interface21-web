af695b02bcdbd822e945ff96c55e5c0ad95e2506
Heiko Scherrer <scherrer@openwms.org>
2016-03-22 23:37:02 +0100

    separated samples into combined and separated Resource and Authorisation server. Have it working to get an access code with curl

---
+++ anthropoda/sample/oauth2/authorizationserver/src/main/java/io/interface21/web/servlet/app/authorization/AuthorizationServerApplication.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.interface21.web.servlet.app.authorization;
+
+import java.util.Collections;
+
+import io.interface21.web.servlet.app.oauth2.EnableOAuth2;
+import org.ameba.annotation.FilteredComponentScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * A AuthorizationServerApplication.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ * @version 1.0
+ * @since 1.0
+ */
+@Configuration
+@EnableAutoConfiguration
+@FilteredComponentScan
+@EnableOAuth2(asAuthorizationServer = true, authenticationProviderBean = "inMemProvider")
+public class AuthorizationServerApplication {
+
+    public
+    @Bean
+    AuthenticationProvider inMemProvider() {
+        DaoAuthenticationProvider dap = new DaoAuthenticationProvider();
+        dap.setPasswordEncoder(new BCryptPasswordEncoder());
+        dap.setUserDetailsService(new InMemoryUserDetailsManager(Collections.singletonList(new User("Username", "Password", Collections.singletonList(new SimpleGrantedAuthority("ADMIN"))))));
+        return dap;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+}
